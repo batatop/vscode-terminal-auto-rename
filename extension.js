89bbcc479e126cae112a26116b63e0423506b40b
batatop <batatop@gmail.com>
2021-10-04 14:39:58 +0300

    commit before release

--- extension.js
+++ extension.js
@@ -1,6 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const pidCwd = require('pid-cwd');
+const path = require('path');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -8,27 +10,38 @@ const vscode = require('vscode');
 /**
  * @param {vscode.ExtensionContext} context
  */
-function activate(context) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "terminal-auto-rename" is now active!');
+let renameTimeout = null
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('terminal-auto-rename.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+function activate() {
+	async function renameWithDirName() {
+		const pid = await vscode.window.activeTerminal.processId
+		pidCwd(pid)
+			.then(cwd => {
+				if(cwd) {
+					const folderName = path.basename(cwd)
+					vscode.commands.executeCommand('workbench.action.terminal.renameWithArg', { name: folderName })
+				}
+			});
+	}
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Terminal Auto Rename!');
+	if(vscode.window.activeTerminal) {
+		renameWithDirName()
+	}
+
+	vscode.window.onDidOpenTerminal(() => {
+		clearTimeout(renameTimeout)
+		renameTimeout = setTimeout(() => {
+			renameWithDirName()
+		}, 400)
 	});
-
-	context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
-function deactivate() {}
+function deactivate() {
+	clearTimeout(renameTimeout)
+	renameTimeout = undefined
+}
 
 module.exports = {
 	activate,
